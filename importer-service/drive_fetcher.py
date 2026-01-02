4eae8a2523d281fa50973776acad1616fba5bac7
Pratham Panchmukh <prathampanchmukh1234@gmail.com>
2026-01-03 00:33:56 +0530

    mock google drive importer

--- importer-service/drive_fetcher.py
+++ importer-service/drive_fetcher.py
@@ -1,29 +1,28 @@
-import os, re
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
+import requests
 
-SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
-CLIENT_SECRET = "client_secret.json"
+SAMPLE_IMAGES = [
+    {
+        "id": "img1",
+        "name": "sample1.jpg",
+        "mimeType": "image/jpeg",
+        "size": 234567,
+        "url": "https://picsum.photos/400"
+    },
+    {
+        "id": "img2",
+        "name": "sample2.jpg",
+        "mimeType": "image/jpeg",
+        "size": 345678,
+        "url": "https://picsum.photos/401"
+    },
+    {
+        "id": "img3",
+        "name": "sample3.jpg",
+        "mimeType": "image/jpeg",
+        "size": 456789,
+        "url": "https://picsum.photos/402"
+    }
+]
 
-def extract_folder_id(url_or_id):
-    if "drive.google.com" in url_or_id:
-        match = re.search(r"folders/([a-zA-Z0-9_-]+)", url_or_id)
-        return match.group(1)
-    return url_or_id.strip()
-
-def get_service():
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET, SCOPES)
-    creds = flow.run_console()
-    return build("drive", "v3", credentials=creds)
-
-def fetch_images_from_drive(folder_input):
-    folder_id = extract_folder_id(folder_input)
-    service = get_service()
-
-    results = service.files().list(
-        q=f"'{folder_id}' in parents and mimeType contains 'image/'",
-        fields="files(id, name, mimeType, size)"
-    ).execute()
-
-    return results.get("files", [])
+def fetch_images_from_drive(folder_url):
+    return SAMPLE_IMAGES
