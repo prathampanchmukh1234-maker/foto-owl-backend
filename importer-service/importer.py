0aee4ce37910b1ad5227a61168ba31000d87935d
Pratham Panchmukh <prathampanchmukh1234@gmail.com>
2026-01-02 11:04:25 +0530

    expose importer api

--- importer-service/importer.py
+++ importer-service/importer.py
@@ -1,18 +1,18 @@
-import redis
-import json
+import redis, json
 from drive_fetcher import fetch_images_from_drive
 
-redis_client = redis.Redis(host="redis", port=6379, decode_responses=True)
+redis_client = redis.Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
 
 QUEUE_NAME = "image_import_queue"
 
 def enqueue_images(folder_url):
     images = fetch_images_from_drive(folder_url)
 
-    print("Fetched images:", images)
+    for image in images:
+        redis_client.rpush(QUEUE_NAME, json.dumps(image))
 
-    for img in images:
-        redis_client.rpush(QUEUE_NAME, json.dumps(img))
-
-    print("Queued", len(images), "images")
     return len(images)
--- importer-service/main.py
+++ importer-service/main.py
@@ -1,17 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 from importer import enqueue_images
 
-app = FastAPI()
+app = FastAPI(title="Foto-Owl Importer Service")
 
 class DriveRequest(BaseModel):
     folder_url: str
 
+
 @app.post("/import")
 def import_images(data: DriveRequest):
-    try:
-        count = enqueue_images(data.folder_url)
-        return {"status": "queued", "images": count}
-    except Exception as e:
-        print("IMPORTER ERROR:", e)
-        raise HTTPException(status_code=500, detail=str(e))
+    count = enqueue_images(data.folder_url)
+    return {"status": "queued", "images": count}
