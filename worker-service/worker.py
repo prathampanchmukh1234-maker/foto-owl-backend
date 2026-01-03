0db78b5ec65a644d2605d38c2296f721513a73a2
Pratham Panchmukh <prathampanchmukh1234@gmail.com>
2026-01-03 21:50:38 +0530

    enable s3 upload worker

--- worker-service/worker.py
+++ worker-service/worker.py
@@ -1,69 +1,28 @@
-import redis
-import json
-import time
-import requests
-import psycopg2
-import os
-from s3_uploader import upload_to_s3
-from db import create_tables, save_metadata
+from upload import upload_image
+import psycopg2, os
 
-REDIS_HOST = "redis"
-REDIS_PORT = 6379
-QUEUE_NAME = "image_import_queue"
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-redis_client = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    decode_responses=True
-)
-
-def wait_for_db():
-    while True:
-        try:
-            conn = psycopg2.connect(DATABASE_URL)
-            conn.close()
-            print("Database is ready")
-            break
-        except psycopg2.OperationalError:
-            print("Waiting for database...")
-            time.sleep(3)
-
-def process_queue():
-    _, image_data = redis_client.blpop(QUEUE_NAME)
-    image = json.loads(image_data)
-
-    file_id = image["id"]
-    download_url = f"https://drive.google.com/uc?id={file_id}"
-
-    response = requests.get(download_url, timeout=15)
-    response.raise_for_status()
-
-    s3_path = upload_to_s3(response.content, image["name"])
-
-    save_metadata({
-        "name": image["name"],
-        "google_drive_id": file_id,
-        "size": image.get("size", 0),
-        "mime_type": image["mimeType"],
-        "storage_path": s3_path
-    })
-
-    print(f"Processed image: {image['name']}")
+DB = os.getenv("DATABASE_URL")
 
 def main():
-    print("Worker service started. Listening for image import jobs...")
+    images = [
+        "https://picsum.photos/400",
+        "https://picsum.photos/401",
+        "https://picsum.photos/402"
+    ]
 
-    wait_for_db()
-    create_tables()
+    conn = psycopg2.connect(DB)
+    cur = conn.cursor()
 
-    while True:
-        try:
-            process_queue()
-        except Exception as e:
-            print(f"Worker error: {e}")
-            time.sleep(2)
+    for i, url in enumerate(images):
+        path = upload_image(url, f"img{i}.jpg")
+        cur.execute(
+            "INSERT INTO images(name, storage_path) VALUES(%s,%s)",
+            (f"img{i}.jpg", path)
+        )
+
+    conn.commit()
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
     main()
