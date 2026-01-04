d3f1c03a715f99d6f91bb33ad6921ff362b06682
Pratham Panchmukh <prathampanchmukh1234@gmail.com>
2026-01-04 22:16:26 +0530

    removed broken docker postgres architecture

--- importer-service/drive_fetcher.py
+++
@@ -1,8 +0,0 @@
-SAMPLE_IMAGES = [
-    {"id": "img1", "name": "sample1.jpg", "mimeType": "image/jpeg", "size": 120000},
-    {"id": "img2", "name": "sample2.jpg", "mimeType": "image/jpeg", "size": 140000},
-    {"id": "img3", "name": "sample3.jpg", "mimeType": "image/jpeg", "size": 160000},
-]
-
-def fetch_images_from_drive(folder_url):
-    return SAMPLE_IMAGES
--- worker-service/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Start the FastAPI app in the background
-uvicorn app:app --host 0.0.0.0 --port 8002 &
-# Start the worker process
-python worker.py
